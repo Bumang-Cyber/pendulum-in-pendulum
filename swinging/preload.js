c7460f7a08728a96f138555c824915f5f027ed68
Bumang-Cyber <jove0729@naver.com>
2024-07-19 02:51:55 +0900

    feat: body overflow hidden으로 스크롤 없애기, 이미지 소스 분리

--- swinging/preload.js
+++ swinging/preload.js
@@ -5,8 +5,10 @@ let layerOuter;
 let layerOuterImage;
 let layerInner;
 let layerInnerImage;
+
 let layerAlt;
 let layerAltImage;
+
 let lensImage;
 let echoSound = new Audio("../asset/audio_echo.mp3");
 
@@ -16,3 +18,39 @@ function preload() {
   layerAltImage = loadImage("../asset/p52.png");
   lensImage = loadImage("../asset/lens.png");
 }
+
+// const IMGWIDTH = 3640;
+// const IMGHEIGHT = 2048;
+
+// let layerOuter;
+// let layerOuterImage;
+// let layerOuterRightUpperImage;
+// let layerOuterLeftUpperImage;
+// let layerOuterCenterImage;
+
+// let layerInner;
+// let layerInnerImage;
+// let layerInnerRightUpperImage;
+// let layerInnerLeftUpperImage;
+// let layerInnerCenterImage;
+
+// let layerAlt;
+// let layerAltImage;
+
+// let lensImage;
+// let echoSound = new Audio("../asset/audio_echo.mp3");
+
+// function preload() {
+//   layerAltImage = loadImage("../asset/p52.png");
+//   lensImage = loadImage("../asset/lens.png");
+
+//   layerInnerImage = loadImage("../asset/p53.png");
+//   layerInnerCenterImage = loadImage("../asset/p53_");
+//   layerInnerLeftUpperImage = loadImage("../asset/p53_rabbit.png");
+//   layerInnerRightUpperImage = loadImage("../asset/p53_racoon.png");
+
+//   layerOuterImage = loadImage("../asset/p51.png");
+//   layerOuterCenterImage = loadImage("../asset/p53_human.png");
+//   layerOuterLeftUpperImage = loadImage("../asset/p53_rabbit.png");
+//   layerOuterRightUpperImage = loadImage("../asset/p53_racoon.png");
+// }
